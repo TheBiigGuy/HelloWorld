4015333a79018c6b1955f24ecdad84f0c76ee26f
Zhaojingwei <1250530035@qq.com>
2016-07-05 13:21:26 +0800

    Initial commit

---
+++ src/demo/HelloWorld.java
@@ -0,0 +1,11 @@
+package demo;
+
+/**
+ * Created by Sam on 2016/7/5.
+ */
+public class HelloWorld {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!");
+    }
+}
